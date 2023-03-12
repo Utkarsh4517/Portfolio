69cc947435fa43b19ebc5b182e2b9e6a8781da69
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-03-13 00:49:12 +0530

    working version before using responsive_framework

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -158,7 +158,7 @@ class HomePage extends StatelessWidget {
                               ),
                             ),
                           ],
-                        )
+                        ),
                       ],
                     ),
                   ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio1/homepage.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() => runApp(const MyApp());
 
